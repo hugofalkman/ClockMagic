fd8681095e236b4e825d9b334d76ddfad84f9303
hugofalkman <hugo@falkman.net>
2018-05-01 00:31:03 +0200

    Empty TableView added

--- ClockMagic/ViewController.swift
+++ ClockMagic/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+
     // MARK: - Properties
     
     @IBOutlet weak var subView: UIView!
+ 
+    @IBOutlet weak var tableView: UITableView!
     
     private var clockView: ClockView? {
         willSet {
@@ -31,6 +33,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         let preferences = Preferences()
         let subview = preferences.model.init(frame: subView.bounds)
         subview.styleName = preferences.styleName
@@ -43,6 +48,18 @@ class ViewController: UIViewController {
         if let clockView = clockView {
             clockView.setNeedsDisplay(clockView.clockFrame)
         }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // let row = indexPath.row
+        cell.textLabel?.text = "test"
+        return cell
     }
 }
 
