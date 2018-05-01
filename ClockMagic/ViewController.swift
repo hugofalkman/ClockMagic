783cc3fff7709160a4263c0f8d7662a7924189f3
hugofalkman <hugo@falkman.net>
2018-05-01 17:09:29 +0200

    Kalendern fungerar i TableView

--- ClockMagic/ViewController.swift
+++ ClockMagic/ViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 import GoogleAPIClientForREST
 import GoogleSignIn
 
-class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate, GIDSignInDelegate, GIDSignInUIDelegate {
 
     // MARK: - Properties
     
     @IBOutlet weak var subView: UIView!
- 
+  
     @IBOutlet weak var tableView: UITableView!
+    
+    private var eventTitle: [String] = []
+    private var eventDetail: [String] = []
     
     private var clockView: ClockView? {
         willSet {
@@ -32,13 +35,32 @@ class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegat
         }
     }
     
+    // If modifying these scopes, delete your previously saved credentials by
+    // resetting the iOS simulator or uninstall the app.
+    private let scopes = [kGTLRAuthScopeCalendarReadonly]
+    
+    private let service = GTLRCalendarService()
+    let signInButton = GIDSignInButton()
+    let output = UITextView()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Configure Google Sign-in.
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().scopes = scopes
+        GIDSignIn.sharedInstance().signInSilently()
+        
+        // Add the sign-in button.
+        tableView.addSubview(signInButton)
+        
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 60
         
         let preferences = Preferences()
         let subview = preferences.model.init(frame: subView.bounds)
@@ -54,18 +76,102 @@ class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegat
         }
     }
     
+    // MARK: - Google ID Signin
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        if let error = error {
+            showAlert(title: "Authentication Error", message: error.localizedDescription)
+            self.service.authorizer = nil
+        } else {
+            self.signInButton.isHidden = true
+            self.output.isHidden = false
+            self.service.authorizer = user.authentication.fetcherAuthorizer()
+            fetchEvents()
+            Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(fetchEvents), userInfo: nil, repeats: true)
+        }
+    }
+    
+    // MARK: - Get Google Calendar Events
+    
+    // Construct a query and get a list of upcoming events from the user calendar
+    @objc func fetchEvents() {
+        let query = GTLRCalendarQuery_EventsList.query(withCalendarId: "primary")
+        // query.maxResults = 10
+        query.timeMin = GTLRDateTime(date: Date())
+        query.singleEvents = true
+        query.orderBy = kGTLRCalendarOrderByStartTime
+        service.executeQuery(
+            query,
+            delegate: self,
+            didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:)))
+    }
+    
+    // MARK: - Display events in TableView
+    
+    // Display the start dates and event summaries in the UITextView
+    @objc func displayResultWithTicket(
+        ticket: GTLRServiceTicket,
+        finishedWithObject response : GTLRCalendar_Events,
+        error : NSError?) {
+        
+        if let error = error {
+            showAlert(title: "Error", message: error.localizedDescription)
+            return
+        }
+        
+        eventTitle = []
+        eventDetail = []
+        if let events = response.items, !events.isEmpty {
+            for event in events {
+                let start = event.start!.dateTime ?? event.start!.date!
+                
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateStyle = .medium
+                dateFormatter.timeStyle = .short
+                dateFormatter.locale = Locale(identifier: "sv")
+                let startString = dateFormatter.string(from: start.date)
+                
+                eventTitle.append(startString + " - " + event.summary!)
+                eventDetail.append(event.descriptionProperty!)
+            }
+        } else {
+            eventTitle = ["Inga kommande händelser"]
+            eventDetail = [""]
+        }
+        tableView.reloadData()
+    }
+    
     // MARK: - TableView Data Source
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return eventTitle.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        // let row = indexPath.row
-        cell.textLabel?.text = "test"
+        let row = indexPath.row
+        cell.textLabel?.text = eventTitle[row]
+        cell.detailTextLabel?.text = eventDetail[row]
         return cell
+    }
+    
+    // MARK: - Showing Alert
+    
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
     }
 }
 
