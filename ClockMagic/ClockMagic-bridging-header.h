4a765c588d2e8e151ab1a5f2bc7509c6bff41161
hugofalkman <hugo@falkman.net>
2018-05-04 13:10:28 +0200

    Updated pods to 1.3.4 and added People API ("Partners"). Added bridging header file to avoid build errors

---
+++ ClockMagic/ClockMagic-bridging-header.h
@@ -0,0 +1,15 @@
+//
+//  ClockMagic-bridging-header.h
+//  ClockMagic
+//
+//  Created by H Hugo Falkman on 2018-05-04.
+//  Copyright © 2018 H Hugo Falkman. All rights reserved.
+//
+
+#ifndef ClockMagic_bridging_header_h
+#define ClockMagic_bridging_header_h
+
+#import <GTMSessionFetcher/GTMSessionFetcher.h>
+#import <GTMSessionFetcher/GTMSessionFetcherService.h>
+
+#endif /* ClockMagic_bridging_header_h */
